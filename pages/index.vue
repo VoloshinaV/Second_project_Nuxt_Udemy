c85ead0ed154b387bc96bd447044f236860d8d87
Viktoria Voloshina <vika13091992@gmail.com>
2023-07-06 16:41:14 -0700

    adding extermal fonts

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
     <section class="featured-posts">
       <nuxt-link to=" '/posts/' + 1">
         <article class="post-preview">
-          <div class="post-thumbnail" style="background-image: url('https://workbloom.com/media/4457/technology.jpg')"></div>
+          <div class="post-thumbnail" style="background-image: url('https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg')"></div>
           <div class="post-content">
             <h1>Post title</h1>
             <p>Preview Text</p>
@@ -15,7 +15,7 @@
       </nuxt-link>
       <nuxt-link to=" '/posts/' + 2">
         <article class="post-preview">
-          <div class="post-thumbnail"></div>
+          <div class="post-thumbnail" style="background-image: url('https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg')"></div>
           <div class="post-content">
             <h1>Post title 2</h1>
             <p>Preview Text 2</p>
@@ -28,6 +28,35 @@
 
 
 <style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
 .post-preview {
   border: 1px solid #ccc;
   box-shadow: 0 2px 2px #ccc;

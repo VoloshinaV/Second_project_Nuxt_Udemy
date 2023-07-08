bbe06fc0107effb174c4fc6517e6d530c2121ebe
Viktoria Voloshina <vika13091992@gmail.com>
2023-07-07 18:27:53 -0700

    Adding Admin Form

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="admin-page">
     <section class="new-post">
-      <button @click="$router.push('/admin/new-post')">Create Post</button>
+      <AppButton @click="$router.push('/admin/new-post')">Create Post</AppButton>
     </section>
     <section class="existing-post">
       <h1>Existing Post</h1>
@@ -12,9 +12,11 @@
 
 <script>
 import PostList from '@/components/Posts/PostList'
+import AppButton from '@/components/UI/AppButton'
 export default {
   components: {
-    PostList
+    PostList,
+    AppButton
   }
 }
 </script>
@@ -30,7 +32,7 @@ export default {
   padding-bottom: 10px;
 }
 
-.existing-posts h1 {
+.existing-post h1 {
   text-align: center;
 }
 </style>

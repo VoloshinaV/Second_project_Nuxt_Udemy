b5405b74fdc60f89143156c0e279514ccf23a4ff
Viktoria Voloshina <vika13091992@gmail.com>
2023-07-10 12:34:35 -0700

    42. Preparing Data on the Server-side Data

--- pages/index.vue
+++ pages/index.vue
@@ -14,26 +14,38 @@ export default {
   components: {
     PostList
   },
-  data() {
-    return {
-      loadedPosts: [
-        { id: '1',
-          title: 'Hello there!',
-          previewText: 'This my first post!',
-          thumbnail: 'https://www.brookings.edu/wp-content/uploads/2022/01/shutterstock_1145553203_small.jpg'
-        },
-        { id: '2',
-          title: 'Hello there - the second time!',
-          previewText: 'This my second post!',
-          thumbnail: 'https://images.ctfassets.net/hrltx12pl8hq/4ACnMj4WVSOZRZt0jHu9h5/1506f652bcd70f4dc3e88219fefea858/shutterstock_739595833-min.jpg?fit=fill&w=600&h=400'
-        },
-        { id: '3',
-          title: 'Hi!',
-          previewText: 'This my third post!',
-          thumbnail: 'https://thumbs.dreamstime.com/z/new-future-technology-concept-abstract-background-business-solution-54350985.jpg'
-        }
-      ]
-    }
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: '1',
+            title: 'Hello there!',
+            previewText: 'This my first post!',
+            thumbnail: 'https://www.brookings.edu/wp-content/uploads/2022/01/shutterstock_1145553203_small.jpg'
+          },
+          {
+            id: '2',
+            title: 'Hello there - the second time!',
+            previewText: 'This my second post!',
+            thumbnail: 'https://images.ctfassets.net/hrltx12pl8hq/4ACnMj4WVSOZRZt0jHu9h5/1506f652bcd70f4dc3e88219fefea858/shutterstock_739595833-min.jpg?fit=fill&w=600&h=400'
+          },
+          {
+            id: '3',
+            title: 'Hi!',
+            previewText: 'This my third post!',
+            thumbnail: 'https://thumbs.dreamstime.com/z/new-future-technology-concept-abstract-background-business-solution-54350985.jpg'
+          }
+        ]
+      }) ;
+    }, 1500);
+  },
+  // data() {
+  //   return {
+  //     loadedPosts: []
+  //   };
+  // },
+  created() {
   }
 }
 </script>

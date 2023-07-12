1d65cf64dcfe86b75a5f5a2a6a2f5b4c526d2c2c
Viktoria Voloshina <vika13091992@gmail.com>
2023-07-11 17:56:54 -0700

    Vuex, fetch(), nuxtServerInit()

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,44 +11,10 @@ export default {
   components: {
     PostList
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-       resolve({
-        loadedPosts: [
-          {
-            id: '1',
-            title: 'Hello there!',
-            previewText: 'This my first post!',
-            thumbnail: 'https://www.brookings.edu/wp-content/uploads/2022/01/shutterstock_1145553203_small.jpg'
-          },
-          {
-            id: '2',
-            title: 'Hello there - the second time!',
-            previewText: 'This my second post!',
-            thumbnail: 'https://images.ctfassets.net/hrltx12pl8hq/4ACnMj4WVSOZRZt0jHu9h5/1506f652bcd70f4dc3e88219fefea858/shutterstock_739595833-min.jpg?fit=fill&w=600&h=400'
-          },
-          {
-            id: '3',
-            title: 'Hi!',
-            previewText: 'This my third post!',
-            thumbnail: 'https://thumbs.dreamstime.com/z/new-future-technology-concept-abstract-background-business-solution-54350985.jpg'
-          }
-        ]
-        });
-      }, 1000);
-      // reject(new Error())
-    })
-    .then(data => {
-      return data
-    })
-    .catch(e => {
-      context.error(e);
-    });
-  },
-  created() {
-    this.$store.dispatch('setPosts', this.loadedPosts)
-    //console.log(this.$store.getters.loadedPosts)
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts
+    }
   }
 };
 </script>
--- store/index.js
+++ store/index.js
@@ -1,26 +1,54 @@
-import Vuex from 'vuex'
+import Vuex from "vuex";
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      loadedPost: []
+      loadedPosts: []
     },
     mutations: {
       setPosts(state, posts) {
-        state.loadedPosts = posts
+        state.loadedPosts = posts;
       }
     },
     actions: {
-      setPosts(vuexContext) {
-        vuexContext.commit('setPosts', posts)
+      nuxtServerInit(vuexContext, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit("setPosts", [
+              {
+                id: '1',
+                title: 'Hello there!',
+                previewText: 'This my first post!',
+                thumbnail: 'https://www.brookings.edu/wp-content/uploads/2022/01/shutterstock_1145553203_small.jpg'
+              },
+              {
+                id: '2',
+                title: 'Hello there - the second time!',
+                previewText: 'This my second post!',
+                thumbnail: 'https://images.ctfassets.net/hrltx12pl8hq/4ACnMj4WVSOZRZt0jHu9h5/1506f652bcd70f4dc3e88219fefea858/shutterstock_739595833-min.jpg?fit=fill&w=600&h=400'
+              },
+              {
+                id: '3',
+                title: 'Hi!',
+                previewText: 'This my third post!',
+                thumbnail: 'https://thumbs.dreamstime.com/z/new-future-technology-concept-abstract-background-business-solution-54350985.jpg'
+              }
+              
+            ]);
+            resolve();
+          }, 1000);
+        });
+      },
+      setPosts(vuexContext, posts) {
+        vuexContext.commit("setPosts", posts);
       }
     },
     getters: {
       loadedPosts(state) {
-        return state.loadedPosts
+        return state.loadedPosts;
       }
     }
-  })
-}
+  });
+};
 
-export default createStore
+export default createStore;

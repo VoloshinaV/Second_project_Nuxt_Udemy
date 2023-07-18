f66f3ea9ec2ee3cdef85a837cddd10b55b89a39b
Viktoria Voloshina <vika13091992@gmail.com>
2023-07-19 01:52:28 +0300

    66. The Nuxt Config File

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'WD Blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'My Web Development Blog'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -25,8 +25,7 @@ module.exports = {
   loading: { color: '#3B8070' },
 
   /*
-  ** Global CSS
-  */
+  **\
   css: [
   ],
 

397d939ff22b4018845e5b13183f7f651de7b530
Viktoria Voloshina <vika13091992@gmail.com>
2023-08-01 00:11:43 -0700

    81. Implementing a Middleware

--- store/index.js
+++ store/index.js
@@ -97,6 +97,9 @@ const createStore = () => {
     getters: {
       loadedPosts(state) {
         return state.loadedPosts;
+      },
+      isAuthenticated(state) {
+        return state.token != null;
       }
     }
   });
